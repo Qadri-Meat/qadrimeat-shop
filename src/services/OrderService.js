3aa8bbfd10dd9922d51a66ed27dbc83b44c34c70
yousaf Ramzan <yousaf.ramzan@devprotocols.com>
2023-06-22 18:20:46 +0500

    working on checkout screen

--- src/services/OrderService.js
+++ src/services/OrderService.js
@@ -2,7 +2,19 @@ import { HTTPBaseService } from "./HTTPBaseService";
 
 class OrderService extends HTTPBaseService {
   create(data) {
-    return this.instance.post(`/v1/orders`, data);
+    const {
+      stock,
+      category,
+      tag,
+      fullDescription,
+      shortDescription,
+      createdAt,
+      id,
+      cartItemId,
+      ...updatedData
+    } = data;
+
+    return this.instance.post(`/v1/orders`, updatedData);
   }
 }
 
