ad1231b8a7d6e88d3f04ca6332dffa2eea706102
yousaf Ramzan <yousaf.ramzan@devprotocols.com>
2023-07-12 15:20:01 +0500

    set shipping page

--- src/pages/other/Shipping.js
+++ src/pages/other/Shipping.js
@@ -1,7 +1,188 @@
-import React from "react";
+import { Fragment } from "react";
+import { Link, useLocation } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { getDiscountPrice } from "../../helpers/product";
+import SEO from "../../components/seo";
+import LayoutOne from "../../layouts/LayoutOne";
+import Breadcrumb from "../../wrappers/breadcrumb/Breadcrumb";
 
-const Shipping = () => {
-  return <div>Shipping</div>;
+const Shipping = ({ customProp }) => {
+  const currency = useSelector((state) => state.currency);
+  const { cartItems } = useSelector((state) => state.cart);
+  const { shippingDetails } = useSelector((state) => state.order.selectedOrder);
+
+  let cartTotalPrice = 0;
+
+  console.log("Props....", customProp);
+
+  let { pathname } = useLocation();
+
+  return (
+    <Fragment>
+      <SEO
+        titleTemplate="Checkout"
+        description="Checkout page of flone react minimalist eCommerce template."
+      />
+      <LayoutOne headerTop="visible">
+        {/* breadcrumb */}
+        <Breadcrumb
+          pages={[
+            { label: "Home", path: process.env.PUBLIC_URL + "/" },
+            { label: "Checkout", path: process.env.PUBLIC_URL + pathname },
+          ]}
+        />
+        <div className="checkout-area pt-95 pb-100">
+          <div className="container">
+            {cartItems && cartItems.length >= 1 ? (
+              <form>
+                <div className="row">
+                  <div className="col-lg-7">
+                    <div className="billing-info-wrap">
+                      <h3>Shipping Order Details</h3>
+                      <div className="col-lg-8">
+                        <div className="your-order-area">
+                          <div className="your-order-wrap gray-bg-4">
+                            <div className="your-order-product-info">
+                              <div className="your-order-top">
+                                <ul>
+                                  <li className="your-order-shipping">Name</li>
+                                  <li>
+                                    {shippingDetails.firstName}{" "}
+                                    {shippingDetails.lastName}
+                                  </li>
+                                </ul>
+                              </div>
+                              <div className="your-order-total">
+                                <ul>
+                                  <li className="your-order-shipping">Phone</li>
+                                  <li>{shippingDetails.phone}</li>
+                                </ul>
+                                <ul>
+                                  <li className="your-order-shipping">
+                                    Address
+                                  </li>
+                                  <li>{shippingDetails.address}</li>
+                                </ul>
+                              </div>
+
+                              <div className="your-order-total">
+                                <ul>
+                                  <li className="your-order-shipping">City</li>
+                                  <li>{shippingDetails.city}</li>
+                                </ul>
+                                <ul>
+                                  <li className="your-order-shipping">
+                                    Country
+                                  </li>
+                                  <li>{shippingDetails.country}</li>
+                                </ul>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-lg-5">
+                    <div className="your-order-area">
+                      <h3>Your order Details</h3>
+                      <div className="your-order-wrap gray-bg-4">
+                        <div className="your-order-product-info">
+                          <div className="your-order-top">
+                            <ul>
+                              <li>Product</li>
+                              <li>Total</li>
+                            </ul>
+                          </div>
+                          <div className="your-order-middle">
+                            <ul>
+                              {cartItems.map((cartItem, key) => {
+                                const discountedPrice = getDiscountPrice(
+                                  cartItem.price,
+                                  cartItem.discount
+                                );
+                                const finalProductPrice = (
+                                  cartItem.price * currency.currencyRate
+                                ).toFixed(2);
+                                const finalDiscountedPrice = (
+                                  discountedPrice * currency.currencyRate
+                                ).toFixed(2);
+                                discountedPrice != null
+                                  ? (cartTotalPrice +=
+                                      finalDiscountedPrice * cartItem.quantity)
+                                  : (cartTotalPrice +=
+                                      finalProductPrice * cartItem.quantity);
+                                return (
+                                  <li key={key}>
+                                    <span className="order-middle-left">
+                                      {cartItem.name} X {cartItem.quantity}
+                                    </span>{" "}
+                                    <span className="order-price">
+                                      {discountedPrice !== null
+                                        ? currency.currencySymbol +
+                                          (
+                                            finalDiscountedPrice *
+                                            cartItem.quantity
+                                          ).toFixed(2)
+                                        : currency.currencySymbol +
+                                          (
+                                            finalProductPrice *
+                                            cartItem.quantity
+                                          ).toFixed(2)}
+                                    </span>
+                                  </li>
+                                );
+                              })}
+                            </ul>
+                          </div>
+                          <div className="your-order-bottom">
+                            <ul>
+                              <li className="your-order-shipping">Shipping</li>
+                              <li>Free shipping</li>
+                            </ul>
+                          </div>
+                          <div className="your-order-total">
+                            <ul>
+                              <li className="order-total">Total</li>
+                              <li>
+                                {currency.currencySymbol +
+                                  cartTotalPrice.toFixed(2)}
+                              </li>
+                            </ul>
+                          </div>
+                        </div>
+                        <div className="payment-method"></div>
+                      </div>
+                      <div className="place-order mt-25">
+                        <button className="btn-hover" type="submit">
+                          Continue to shipping
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </form>
+            ) : (
+              <div className="row">
+                <div className="col-lg-12">
+                  <div className="item-empty-area text-center">
+                    <div className="item-empty-area__icon mb-30">
+                      <i className="pe-7s-cash"></i>
+                    </div>
+                    <div className="item-empty-area__text">
+                      No items found in cart to checkout <br />{" "}
+                      <Link to={process.env.PUBLIC_URL + "/shop"}>
+                        Shop Now
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
+      </LayoutOne>
+    </Fragment>
+  );
 };
-
 export default Shipping;
