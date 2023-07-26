3f25474f1a471302d2b0ce83c5449be458d78453
yousaf Ramzan <yousaf.ramzan@devprotocols.com>
2023-07-26 15:34:38 +0500

    change the design of order detail screen

--- src/pages/other/Order.js
+++ src/pages/other/Order.js
@@ -13,6 +13,7 @@ const Order = () => {
   let { id } = useParams();
   const currency = useSelector((state) => state.currency);
   const { details: order } = useSelector((state) => state.order);
+  let cartTotalPrice = 0;
 
   useEffect(() => {
     dispatch(getOrder(id));
@@ -33,148 +34,171 @@ const Order = () => {
             { label: "Order", path: process.env.PUBLIC_URL + pathname },
           ]}
         />
-        {order && (
-          <div className="cart-main-area pt-90 pb-100">
-            <div className="container">
-              {order.orderItems.length >= 1 ? (
-                <Fragment>
-                  <h3 className="cart-page-title">Your cart items</h3>
-                  <div className="row">
-                    <div className="col-7">
-                      <div className="table-content table-responsive cart-table-content">
-                        <table>
-                          <thead>
-                            <tr>
-                              <th>Image</th>
-                              <th>Product Name</th>
-                              <th>Unit Price</th>
-                              <th>Subtotal</th>
-                            </tr>
-                          </thead>
-                          <tbody>
-                            {order.orderItems.map((orderItem, key) => {
-                              const discountedPrice = getDiscountPrice(
-                                orderItem.price,
-                                orderItem.discount
-                              );
-                              const finalProductPrice = (
-                                orderItem.price * currency.currencyRate
-                              ).toFixed(2);
-                              const finalDiscountedPrice = (
-                                discountedPrice * currency.currencyRate
-                              ).toFixed(2);
-                              return (
-                                <tr key={key}>
-                                  <td className="product-thumbnail">
-                                    <Link
-                                      to={
-                                        process.env.PUBLIC_URL +
-                                        "/product/" +
-                                        orderItem.product
-                                      }
-                                    >
-                                      <img
-                                        className="img-fluid"
-                                        src={
-                                          process.env.REACT_APP_IMAGE_URL +
-                                          orderItem.image[0]
-                                        }
-                                        alt=""
-                                      />
-                                    </Link>
-                                  </td>
-
-                                  <td className="product-name">
-                                    <Link
-                                      to={
-                                        process.env.PUBLIC_URL +
-                                        "/product/" +
-                                        orderItem.product
-                                      }
-                                    >
-                                      {orderItem.name}
-                                    </Link>
-                                  </td>
-
-                                  <td className="product-price-cart">
-                                    {discountedPrice !== null ? (
-                                      <Fragment>
-                                        <span className="amount old">
-                                          {currency.currencySymbol +
-                                            finalProductPrice}
-                                        </span>
-                                        <span className="amount">
-                                          {currency.currencySymbol +
-                                            finalDiscountedPrice}
-                                        </span>
-                                      </Fragment>
-                                    ) : (
-                                      <span className="amount">
-                                        {currency.currencySymbol +
-                                          finalProductPrice}
-                                      </span>
-                                    )}
-                                  </td>
-                                  <td className="product-subtotal">
-                                    {discountedPrice !== null
-                                      ? currency.currencySymbol +
-                                        (
-                                          finalDiscountedPrice *
-                                          orderItem.quantity
-                                        ).toFixed(2)
-                                      : currency.currencySymbol +
-                                        (
-                                          finalProductPrice * orderItem.quantity
-                                        ).toFixed(2)}
-                                  </td>
+        <div className="cart-main-area pt-90 pb-100">
+          <div className="container">
+            <Fragment>
+              {order && order.orderItems.length >= 1 ? (
+                <div className="cart-main-area pt-90 pb-100">
+                  <div className="container">
+                    <Fragment>
+                      <h3 className="cart-page-title">
+                        {order.shippingDetails.firstName}{" "}
+                        {order.shippingDetails.lastName}
+                      </h3>
+                      <h5 className="cart-page-title">
+                        Address {order.shippingDetails.address}
+                      </h5>
+                      <h5 className="cart-page-title">
+                        Phone {order.shippingDetails.phone}
+                      </h5>
+                      <div className="row">
+                        <div className="col-12">
+                          <div className="table-content table-responsive cart-table-content">
+                            <table>
+                              <thead>
+                                <tr>
+                                  <th>Image</th>
+                                  <th>Product Name</th>
+                                  <th>Unit Price</th>
+                                  <th>Subtotal</th>
                                 </tr>
-                              );
-                            })}
-                          </tbody>
-                        </table>
-                      </div>
-                    </div>
-                    <div className="col-5">
-                      <div className="your-order-area">
-                        <div className="your-order-wrap gray-bg-4">
-                          <div className="your-order-product-info">
-                            <div className="your-order-top">
-                              <ul>
-                                <li>Recipient</li>
-                                <li>
-                                  {order.shippingDetails.firstName}{" "}
-                                  {order.shippingDetails.lastName}
-                                </li>
-                              </ul>
-                            </div>
-                            <div className="your-order-middle">
-                              <ul>
-                                <li>Phone Number</li>
-                                <li>{order.shippingDetails.phone}</li>
-                              </ul>
-                            </div>
-                            <div className="your-order-middle">
-                              <ul>
-                                <li>Shipping Address</li>
-                                <li>{order.shippingDetails.address}</li>
-                              </ul>
-                            </div>
-                            <div className="your-order-total">
-                              <ul>
-                                <li className="order-total">Total</li>
-                                <li>
-                                  {currency.currencySymbol +
-                                    order.totalPrice.toFixed(2)}
-                                </li>
-                              </ul>
-                            </div>
+                              </thead>
+                              <tbody>
+                                {order.orderItems.map((orderItem, key) => {
+                                  const discountedPrice = getDiscountPrice(
+                                    orderItem.price,
+                                    orderItem.discount
+                                  );
+                                  const finalProductPrice = (
+                                    orderItem.price * currency.currencyRate
+                                  ).toFixed(2);
+                                  const finalDiscountedPrice = (
+                                    discountedPrice * currency.currencyRate
+                                  ).toFixed(2);
+
+                                  discountedPrice != null
+                                    ? (cartTotalPrice +=
+                                        finalDiscountedPrice *
+                                        orderItem.quantity)
+                                    : (cartTotalPrice +=
+                                        finalProductPrice * orderItem.quantity);
+                                  return (
+                                    <tr key={key}>
+                                      <td className="product-thumbnail">
+                                        <Link
+                                          to={
+                                            process.env.PUBLIC_URL +
+                                            "/product/" +
+                                            orderItem.id
+                                          }
+                                        >
+                                          <img
+                                            className="img-fluid"
+                                            src={
+                                              process.env.REACT_APP_IMAGE_URL +
+                                              orderItem.image[0]
+                                            }
+                                            alt=""
+                                          />
+                                        </Link>
+                                      </td>
+
+                                      <td className="product-name">
+                                        <Link
+                                          to={
+                                            process.env.PUBLIC_URL +
+                                            "/product/" +
+                                            orderItem.id
+                                          }
+                                        >
+                                          {orderItem.name}
+                                        </Link>
+                                        {orderItem.selectedProductColor &&
+                                        orderItem.selectedProductSize ? (
+                                          <div className="cart-item-variation">
+                                            <span>
+                                              Color:{" "}
+                                              {orderItem.selectedProductColor}
+                                            </span>
+                                            <span>
+                                              Size:{" "}
+                                              {orderItem.selectedProductSize}
+                                            </span>
+                                          </div>
+                                        ) : (
+                                          ""
+                                        )}
+                                      </td>
+
+                                      <td className="product-price-cart">
+                                        {discountedPrice !== null ? (
+                                          <Fragment>
+                                            <span className="amount old">
+                                              {currency.currencySymbol +
+                                                finalProductPrice}
+                                            </span>
+                                            <span className="amount">
+                                              {currency.currencySymbol +
+                                                finalDiscountedPrice}
+                                            </span>
+                                          </Fragment>
+                                        ) : (
+                                          <span className="amount">
+                                            {currency.currencySymbol +
+                                              finalProductPrice}
+                                          </span>
+                                        )}
+                                      </td>
+                                      <td className="product-subtotal">
+                                        {discountedPrice !== null
+                                          ? currency.currencySymbol +
+                                            (
+                                              finalDiscountedPrice *
+                                              orderItem.quantity
+                                            ).toFixed(2)
+                                          : currency.currencySymbol +
+                                            (
+                                              finalProductPrice *
+                                              orderItem.quantity
+                                            ).toFixed(2)}
+                                      </td>
+                                    </tr>
+                                  );
+                                })}
+                              </tbody>
+                            </table>
                           </div>
-                          <div className="payment-method"></div>
                         </div>
                       </div>
-                    </div>
+                      <div className="row">
+                        <div className="col-lg-4 col-md-12">
+                          <div className="grand-totall">
+                            <div className="title-wrap">
+                              <h4 className="cart-bottom-title section-bg-gary-cart">
+                                Receipt
+                              </h4>
+                            </div>
+                            <h5>
+                              Total products{" "}
+                              <span>
+                                {currency.currencySymbol +
+                                  cartTotalPrice.toFixed(2)}
+                              </span>
+                            </h5>
+
+                            <h4 className="grand-totall-title">
+                              Grand Total{" "}
+                              <span>
+                                {currency.currencySymbol +
+                                  cartTotalPrice.toFixed(2)}
+                              </span>
+                            </h4>
+                          </div>
+                        </div>
+                      </div>
+                    </Fragment>
                   </div>
-                </Fragment>
+                </div>
               ) : (
                 <div className="row">
                   <div className="col-lg-12">
@@ -192,9 +216,9 @@ const Order = () => {
                   </div>
                 </div>
               )}
-            </div>
+            </Fragment>
           </div>
-        )}
+        </div>
       </LayoutOne>
     </Fragment>
   );
