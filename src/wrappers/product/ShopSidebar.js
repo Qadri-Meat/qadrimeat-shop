f4736cb5582bae5fffb3cda62a9252516af194e1
yousaf Ramzan <yousaf.ramzan@devprotocols.com>
2023-07-26 16:15:17 +0500

    Hide color and size filter from shop page

--- src/wrappers/product/ShopSidebar.js
+++ src/wrappers/product/ShopSidebar.js
@@ -3,19 +3,13 @@ import clsx from "clsx";
 import {
   getIndividualCategories,
   getIndividualTags,
-  getIndividualColors,
-  getProductsIndividualSizes
 } from "../../helpers/product";
 import ShopSearch from "../../components/product/ShopSearch";
 import ShopCategories from "../../components/product/ShopCategories";
-import ShopColor from "../../components/product/ShopColor";
-import ShopSize from "../../components/product/ShopSize";
 import ShopTag from "../../components/product/ShopTag";
 
 const ShopSidebar = ({ products, getSortParams, sideSpaceClass }) => {
   const uniqueCategories = getIndividualCategories(products);
-  const uniqueColors = getIndividualColors(products);
-  const uniqueSizes = getProductsIndividualSizes(products);
   const uniqueTags = getIndividualTags(products);
 
   return (
@@ -29,12 +23,6 @@ const ShopSidebar = ({ products, getSortParams, sideSpaceClass }) => {
         getSortParams={getSortParams}
       />
 
-      {/* filter by color */}
-      <ShopColor colors={uniqueColors} getSortParams={getSortParams} />
-
-      {/* filter by size */}
-      <ShopSize sizes={uniqueSizes} getSortParams={getSortParams} />
-
       {/* filter by tag */}
       <ShopTag tags={uniqueTags} getSortParams={getSortParams} />
     </div>
@@ -44,7 +32,7 @@ const ShopSidebar = ({ products, getSortParams, sideSpaceClass }) => {
 ShopSidebar.propTypes = {
   getSortParams: PropTypes.func,
   products: PropTypes.array,
-  sideSpaceClass: PropTypes.string
+  sideSpaceClass: PropTypes.string,
 };
 
 export default ShopSidebar;
