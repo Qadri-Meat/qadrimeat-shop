ed6859901280a36dc4aeeed5157612dd18672a73
yousaf Ramzan <yousaf.ramzan@devprotocols.com>
2023-07-27 15:50:18 +0500

    Make nessesary changes in the mobile view navbar

--- src/components/header/sub-components/MobileWidgets.js
+++ src/components/header/sub-components/MobileWidgets.js
@@ -1,5 +1,3 @@
-
-
 const MobileWidgets = () => {
   return (
     <div className="offcanvas-widget-area">
@@ -8,28 +6,22 @@ const MobileWidgets = () => {
           <ul className="header-contact-info__list">
             <li>
               <i className="fa fa-phone"></i>{" "}
-              <a href="tel://12452456012">(1245) 2456 012 </a>
+              <a href="tel://+92 304 4014345">+92 304 4014345 </a>
             </li>
             <li>
               <i className="fa fa-envelope"></i>{" "}
-              <a href="mailto:info@yourdomain.com">info@yourdomain.com</a>
+              <a href="mailto:qadrimeat@gmail.com">qadrimeat@gmail.com</a>
             </li>
           </ul>
         </div>
       </div>
       {/*Off Canvas Widget Social Start*/}
       <div className="off-canvas-widget-social">
-        <a href="//twitter.com" title="Twitter">
-          <i className="fa fa-twitter"></i>
-        </a>
         <a href="//instagram.com" title="Instagram">
           <i className="fa fa-instagram"></i>
         </a>
         <a href="//facebook.com" title="Facebook">
           <i className="fa fa-facebook"></i>
-        </a>
-        <a href="//pinterest.com" title="Pinterest">
-          <i className="fa fa-pinterest"></i>
         </a>
       </div>
       {/*Off Canvas Widget Social End*/}
