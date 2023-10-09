89bddd58d913fe78c293b6ef908fd8531769d00c
yousaf Ramzan <yousaf.ramzan@devprotocols.com>
2023-10-09 15:38:17 +0500

    test: Checkout button is not showing properly issue fixed

--- src/components/header/IconGroup.js
+++ src/components/header/IconGroup.js
@@ -1,39 +1,43 @@
-import PropTypes from "prop-types";
-import { Link } from "react-router-dom";
-import { useSelector } from "react-redux";
-import clsx from "clsx";
-import MenuCart from "./sub-components/MenuCart";
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import clsx from 'clsx';
+import MenuCart from './sub-components/MenuCart';
 
 const IconGroup = ({ iconWhiteClass }) => {
   const handleClick = (e) => {
-    e.currentTarget.nextSibling.classList.toggle("active");
+    e.currentTarget.nextSibling.classList.toggle('active');
   };
 
   const triggerMobileMenu = () => {
     const offcanvasMobileMenu = document.querySelector(
-      "#offcanvas-mobile-menu"
+      '#offcanvas-mobile-menu'
     );
-    offcanvasMobileMenu.classList.add("active");
+    offcanvasMobileMenu.classList.add('active');
   };
   const { compareItems } = useSelector((state) => state.compare);
   const { wishlistItems } = useSelector((state) => state.wishlist);
   const { cartItems } = useSelector((state) => state.cart);
 
   return (
-    <div className={clsx("header-right-wrap", iconWhiteClass)}>
+    <div className={clsx('header-right-wrap', iconWhiteClass)}>
       <div className="same-style header-compare">
-        <Link to={process.env.PUBLIC_URL + "/compare"}>
+        <Link to={process.env.PUBLIC_URL + '/compare'}>
           <i className="pe-7s-shuffle" />
           <span className="count-style">
-            {compareItems && compareItems.length ? compareItems.length : 0}
+            {compareItems && compareItems.length
+              ? compareItems.length
+              : 0}
           </span>
         </Link>
       </div>
       <div className="same-style header-wishlist">
-        <Link to={process.env.PUBLIC_URL + "/wishlist"}>
+        <Link to={process.env.PUBLIC_URL + '/wishlist'}>
           <i className="pe-7s-like" />
           <span className="count-style">
-            {wishlistItems && wishlistItems.length ? wishlistItems.length : 0}
+            {wishlistItems && wishlistItems.length
+              ? wishlistItems.length
+              : 0}
           </span>
         </Link>
       </div>
@@ -48,7 +52,10 @@ const IconGroup = ({ iconWhiteClass }) => {
         <MenuCart />
       </div>
       <div className="same-style cart-wrap d-block d-lg-none">
-        <Link className="icon-cart" to={process.env.PUBLIC_URL + "/cart"}>
+        <Link
+          className="icon-cart"
+          to={process.env.PUBLIC_URL + '/cart'}
+        >
           <i className="pe-7s-shopbag" />
           <span className="count-style">
             {cartItems && cartItems.length ? cartItems.length : 0}
--- src/components/header/sub-components/MenuCart.js
+++ src/components/header/sub-components/MenuCart.js
@@ -1,8 +1,8 @@
-import { Fragment } from "react";
-import { Link } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux";
-import { getDiscountPrice } from "../../../helpers/product";
-import { deleteFromCart } from "../../../store/slices/cart-slice";
+import { Fragment } from 'react';
+import { Link } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { getDiscountPrice } from '../../../helpers/product';
+import { deleteFromCart } from '../../../store/slices/cart-slice';
 
 const MenuCart = () => {
   const dispatch = useDispatch();
@@ -14,82 +14,124 @@ const MenuCart = () => {
     <div className="shopping-cart-content">
       {cartItems && cartItems.length > 0 ? (
         <Fragment>
-          <ul>
-            {cartItems.map((item) => {
-              const discountedPrice = getDiscountPrice(
-                item.price,
-                item.discount
-              );
-              const finalProductPrice = (
-                item.price * currency.currencyRate
-              ).toFixed(2);
-              const finalDiscountedPrice = (
-                discountedPrice * currency.currencyRate
-              ).toFixed(2);
+          <div className="shopping-cart-items">
+            <ul
+              style={{
+                maxHeight: '250px',
+                overflowY: 'auto',
+                paddingRight: '20px',
+              }}
+            >
+              {cartItems.map((item) => {
+                const discountedPrice = getDiscountPrice(
+                  item.price,
+                  item.discount
+                );
+                const finalProductPrice = (
+                  item.price * currency.currencyRate
+                ).toFixed(2);
+                const finalDiscountedPrice = (
+                  discountedPrice * currency.currencyRate
+                ).toFixed(2);
 
-              discountedPrice != null
-                ? (cartTotalPrice += finalDiscountedPrice * item.quantity)
-                : (cartTotalPrice += finalProductPrice * item.quantity);
+                discountedPrice != null
+                  ? (cartTotalPrice +=
+                      finalDiscountedPrice * item.quantity)
+                  : (cartTotalPrice +=
+                      finalProductPrice * item.quantity);
 
-              return (
-                <li className="single-shopping-cart" key={item.cartItemId}>
-                  <div className="shopping-cart-img">
-                    <Link to={process.env.PUBLIC_URL + "/product/" + item.id}>
-                      <img
-                        alt=""
-                        src={process.env.REACT_APP_IMAGE_URL + item.image[0]}
-                        className="img-fluid"
-                      />
-                    </Link>
-                  </div>
-                  <div className="shopping-cart-title">
-                    <h4>
-                      <Link to={process.env.PUBLIC_URL + "/product/" + item.id}>
-                        {" "}
-                        {item.name}{" "}
+                return (
+                  <li
+                    className="single-shopping-cart"
+                    key={item.cartItemId}
+                  >
+                    <div className="shopping-cart-img">
+                      <Link
+                        to={
+                          process.env.PUBLIC_URL +
+                          '/product/' +
+                          item.id
+                        }
+                      >
+                        <img
+                          alt=""
+                          src={
+                            process.env.REACT_APP_IMAGE_URL +
+                            item.image[0]
+                          }
+                          className="img-fluid"
+                        />
                       </Link>
-                    </h4>
-                    <h6>Qty: {item.quantity}</h6>
-                    <span>
-                      {discountedPrice !== null
-                        ? currency.currencySymbol + finalDiscountedPrice
-                        : currency.currencySymbol + finalProductPrice}
-                    </span>
-                    {item.selectedProductColor && item.selectedProductSize ? (
-                      <div className="cart-item-variation">
-                        <span>Color: {item.selectedProductColor}</span>
-                        <span>Size: {item.selectedProductSize}</span>
-                      </div>
-                    ) : (
-                      ""
-                    )}
-                  </div>
-                  <div className="shopping-cart-delete">
-                    <button
-                      onClick={() => dispatch(deleteFromCart(item.cartItemId))}
-                    >
-                      <i className="fa fa-times-circle" />
-                    </button>
-                  </div>
-                </li>
-              );
-            })}
-          </ul>
+                    </div>
+                    <div className="shopping-cart-title">
+                      <h4>
+                        <Link
+                          to={
+                            process.env.PUBLIC_URL +
+                            '/product/' +
+                            item.id
+                          }
+                        >
+                          {' '}
+                          {item.name}{' '}
+                        </Link>
+                      </h4>
+                      <h6>Qty: {item.quantity}</h6>
+                      <span>
+                        {discountedPrice !== null
+                          ? currency.currencySymbol +
+                            finalDiscountedPrice
+                          : currency.currencySymbol +
+                            finalProductPrice}
+                      </span>
+                      {item.selectedProductColor &&
+                      item.selectedProductSize ? (
+                        <div className="cart-item-variation">
+                          <span>
+                            Color: {item.selectedProductColor}
+                          </span>
+                          <span>
+                            Size: {item.selectedProductSize}
+                          </span>
+                        </div>
+                      ) : (
+                        ''
+                      )}
+                    </div>
+                    <div className="shopping-cart-delete">
+                      <button
+                        onClick={() =>
+                          dispatch(deleteFromCart(item.cartItemId))
+                        }
+                      >
+                        <i className="fa fa-times-circle" />
+                      </button>
+                    </div>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+
           <div className="shopping-cart-total">
             <h4>
-              Total :{" "}
+              Total :{' '}
               <span className="shop-total">
                 {currency.currencySymbol + cartTotalPrice.toFixed(2)}
               </span>
             </h4>
           </div>
+
           <div className="shopping-cart-btn btn-hover text-center">
-            <Link className="default-btn" to={process.env.PUBLIC_URL + "/cart"}>
+            <Link
+              className="default-btn"
+              to={process.env.PUBLIC_URL + '/cart'}
+            >
               view cart
             </Link>
             <Link
               className="default-btn"
-              to={process.env.PUBLIC_URL + "/checkout"}
+              to={process.env.PUBLIC_URL + '/checkout'}
             >
               checkout
             </Link>
